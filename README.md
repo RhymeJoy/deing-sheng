ec6a4281bd1594eb7121f8fab22b69b04bdae1a5
RhymeJoy <t40932203@gmail.com>
2026-07-07 11:19:02 +0800

    reconst nuxt

--- README.md
+++ README.md
@@ -1,23 +1,6 @@
 # deing-sheng
 
-This template should help get you started developing with Vue 3 in Vite.
-
-## Recommended IDE Setup
-
-[VS Code](https://code.visualstudio.com/) + [Vue (Official)](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur).
-
-## Recommended Browser Setup
-
-- Chromium-based browsers (Chrome, Edge, Brave, etc.):
-  - [Vue.js devtools](https://chromewebstore.google.com/detail/vuejs-devtools/nhdogjmejiglipccpnnnanhbledajbpd)
-  - [Turn on Custom Object Formatter in Chrome DevTools](http://bit.ly/object-formatters)
-- Firefox:
-  - [Vue.js devtools](https://addons.mozilla.org/en-US/firefox/addon/vue-js-devtools/)
-  - [Turn on Custom Object Formatter in Firefox DevTools](https://fxdx.dev/firefox-devtools-custom-object-formatters/)
-
-## Customize configuration
-
-See [Vite Configuration Reference](https://vite.dev/config/).
+Nuxt-based company website for Deing Sheng.
 
 ## Project Setup
 
@@ -37,8 +20,20 @@ npm run dev
 npm run build
 ```
 
-### Lint with [ESLint](https://eslint.org/)
+### Preview Production Build
+
+```sh
+npm run preview
+```
+
+### Lint with ESLint
 
 ```sh
 npm run lint
+```
+
+### Typecheck
+
+```sh
+npm run typecheck
 ```
--- app/App.vue
+++ app/App.vue
@@ -1,30 +1,28 @@
-<<<<<<< HEAD
 <script setup lang="ts">
 import { onMounted, watch } from 'vue'
 import { useRoute } from '#app'
 import { useI18n } from 'vue-i18n'
 
 import { usePageSeo } from '~/composables/usePageSeo'
+
 const { locale } = useI18n()
 const route = useRoute()
 
 usePageSeo()
-=======
-<script setup>
-import Navbar from './components/Navbar.vue'
-import Footer from './components/Footer.vue'
->>>>>>> 88b71cf82d345003a84c693793d36f0c0d350582
 
 function scrollToTopBeforeEnter() {
+  if (!import.meta.client) return
+
   window.scrollTo(0, 0)
   document.documentElement.scrollTop = 0
   document.body.scrollTop = 0
 }
-<<<<<<< HEAD
 
 function normalizeLocale(value: unknown) {
-  return value === 'en' || value === 'zh-TW'
-    ? value
+  const localeValue = Array.isArray(value) ? value[0] : value
+
+  return localeValue === 'en' || localeValue === 'zh-TW'
+    ? localeValue
     : null
 }
 
@@ -53,32 +51,23 @@ onMounted(() => {
 })
 
 watch(locale, (value) => {
+  if (!import.meta.client) return
+
   document.documentElement.lang = value
   localStorage.setItem('lang', value)
 })
-=======
->>>>>>> 88b71cf82d345003a84c693793d36f0c0d350582
 </script>
 
 <template>
   <div id="page-wrapper">
-<<<<<<< HEAD
     <Navbar />
 
     <NuxtPage v-slot="{ Component, route: pageRoute }">
-=======
-
-    <Navbar />
-
-    <RouterView v-slot="{ Component, route }">
-
->>>>>>> 88b71cf82d345003a84c693793d36f0c0d350582
       <Transition
         name="page"
         mode="out-in"
         @before-enter="scrollToTopBeforeEnter"
       >
-<<<<<<< HEAD
         <component
           :is="Component"
           :key="pageRoute.fullPath"
@@ -91,24 +80,6 @@ watch(locale, (value) => {
 </template>
 
 <style src="~/assets/css/leaflet.css"></style>
-=======
-
-        <component
-          :is="Component"
-          :key="route.fullPath"
-        />
-
-      </Transition>
-
-    </RouterView>
-
-    <Footer />
-
-  </div>
-</template>
-
-<style src="./assets/css/leaflet.css"></style>
->>>>>>> 88b71cf82d345003a84c693793d36f0c0d350582
 
 <style>
 .page-enter-active,
