34ffd9a3eb978212766172f4ecce67b689f0fc6a
RhymeJoy <t40932203@gmail.com>
2026-07-07 09:58:55 +0800

    update sources

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,10 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig(({ command }) => ({
+  base: '/deing-sheng/',
   plugins: [
     vue(),
     command === 'serve' ? vueDevTools() : null,
